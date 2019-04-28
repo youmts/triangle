0cd8c35b258497189246bd734e8489ef13abf377
You Matsumoto <youmts22@gmail.com>
2019-04-28 11:23:18 +0900

    Make what_kind_of method.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,6 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
+  specify { expect(Triangle.what_kind_of(0, 1, 1)).to eq :not}
+  specify { expect(Triangle.what_kind_of(1, 0, 1)).to eq :not}
+  specify { expect(Triangle.what_kind_of(1, 1, 0)).to eq :not}
+
   specify { expect(Triangle.what_kind_of(2, 3, 4)).to eq :normal}
   specify { expect(Triangle.what_kind_of(2, 2, 1)).to eq :isosceles}
   specify { expect(Triangle.what_kind_of(1, 1, 1)).to eq :equilateral}
--- triangle.rb
+++ triangle.rb
@@ -1,5 +1,22 @@
 class Triangle
   def self.what_kind_of(a, b, c)
+    # 辺の長さが0以下は三角形でない
+    return :not if a <= 0
+    return :not if b <= 0
+    return :not if c <= 0
+
+    # 任意の二辺の長さの合計が残りの辺より短いと三角形でない
+    return :not if a + b <= c
+    return :not if b + c <= a
+    return :not if c + a <= b
+
+    # 正三角形
+    return :equilateral if a == b && b == c
+
+    # 二等辺三角形
+    return :isosceles if a == b || b == c || c == a
+
+    # 普通の三角形
     :normal
   end
 end
