2a168b65376959267adda64f55bcffd2063e7342
You Matsumoto <youmts22@gmail.com>
2019-04-28 11:58:39 +0900

    Improve test cases.

--- spec/triangle_spec.rb
+++ spec/triangle_spec.rb
@@ -1,12 +1,31 @@
 require File.expand_path(File.dirname(__FILE__) + '/../triangle')
 
 describe Triangle do
-  specify { expect(Triangle.what_kind_of(0, 1, 1)).to eq :not}
-  specify { expect(Triangle.what_kind_of(1, 0, 1)).to eq :not}
-  specify { expect(Triangle.what_kind_of(1, 1, 0)).to eq :not}
+  specify "いずれかの辺が0" do
+    [0, 1, 1].combination(3) { |sides|
+      expect(Triangle.what_kind_of(*sides)).to eq :not
+    }
+  end
 
-  specify { expect(Triangle.what_kind_of(2, 3, 4)).to eq :normal}
-  specify { expect(Triangle.what_kind_of(2, 2, 1)).to eq :isosceles}
-  specify { expect(Triangle.what_kind_of(1, 1, 1)).to eq :equilateral}
-  specify { expect(Triangle.what_kind_of(1, 2, 3)).to eq :not}
+  specify "2+2+2の正三角形" do
+    expect(Triangle.what_kind_of(2, 2, 2)).to eq :equilateral
+  end
+
+  specify "2+2+3の二等辺三角形" do
+    [2,2,3].combination(3) {|sides|
+      expect(Triangle.what_kind_of(*sides)).to eq :isosceles
+    }
+  end
+
+  specify "2+2+4の非三角形" do
+    [2,2,4].combination(3) {|sides|
+      expect(Triangle.what_kind_of(*sides)).to eq :not
+    }
+  end
+
+  specify "2+3+4の三角形" do
+    [2,3,4].combination(3) {|sides|
+      expect(Triangle.what_kind_of(*sides)).to eq :normal
+    }
+  end
 end
